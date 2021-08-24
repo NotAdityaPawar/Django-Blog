03f6196914b69bb044bc4f48313c10ed99e0c648
arvind pawar <pawar97ankur@outlook.com>
2021-08-24 21:22:43 +0530

    Added the template for blog list

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -4,7 +4,11 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField()
+    title = models.CharField(max_length = 255)
     body = models.TextField()
     created_by = models.ForeignKey(User,on_delete=models.CASCADE)
     date_added = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return self.title
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+class PostListView(generic.ListView):
+    model = Post
+    template_name = "post_list.html"
